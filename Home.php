f65892d0124f5a6d94a1a804275105fbf07e331c
Alwen <a0936566213@gmail.com>
2018-11-04 23:40:29 +0800

    home

--- Home.php
+++ Home.php
@@ -14,7 +14,7 @@
 
 
 body {
-     /* background: #000 url(img/home.png) center center fixed no-repeat; */
+     background: #000 url(img/home.png) center center fixed no-repeat;
      
      background-position:center-center;
      -webkit-background-size:cover;
@@ -141,7 +141,7 @@ img{
     }
 </style>
 </head>
-<body background="img/home.png">
+<body>
 <header class="htmleaf-header">
 <div class="t">
   <img src="img/e.png">
