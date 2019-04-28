0dc782189eb138e949b7aad95f35a1b07d2679e5
hsa90807 <405410423@s05.tku.edu.tw>
2019-04-28 13:48:56 +0800

    4/28-A

--- Topic.php
+++ Topic.php
@@ -14,7 +14,7 @@ include("mysql_connect.php");
  ?>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta name="viewport" content="width=device-width;initial-scale=1.0;user-scalable=0;">
+	<meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=0">
 	<link rel="stylesheet" type="text/css" href="css/default.css">
 	<link rel="stylesheet" href="css/style.css" type="text/css" />
 	<title>首頁</title>
