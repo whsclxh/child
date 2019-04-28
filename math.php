27b80a4908f313d8be877f10cddd9e93b719e7f6
hsa90807 <405410423@s05.tku.edu.tw>
2019-04-28 14:55:50 +0800

    4/28-F

--- math.php
+++ math.php
@@ -644,6 +644,7 @@ require "math_triple_add_function.php";
 </head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://apps.bdimg.com/libs/jquerymobile/1.4.5/jquery.mobile-1.4.5.min.css">
+<link rel="stylesheet" href=".css/math_rwd">
 <script src="https://apps.bdimg.com/libs/jquery/1.10.2/jquery.min.js"></script>
 <script src="https://apps.bdimg.com/libs/jquerymobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
 <script type="text/javascript" src="./math_js/math_add_sub.js"></script>
