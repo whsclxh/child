309caca9ef8a1b3fc990d819b3c0199df771518d
hsa90807 <[hsa90807@gmail.com]>
2019-07-10 12:45:53 +0800

    7/10-Q2

--- green/receive.php
+++ green/receive.php
@@ -5,7 +5,22 @@ require_once 'ECPay.Payment.Integration.php';
 define( 'ECPay_MerchantID', '2000132' );
 define( 'ECPay_HashKey', '5294y06JbISpM5x9' );
 define( 'ECPay_HashIV', 'v77hoKGq4kWxNNIS' );
-
+$CustomField1=$_POST['CustomField1'];
+$CustomField2=$_POST['CustomField2'];
+$CustomField3=$_POST['CustomField3'];
+$CustomField4=$_POST['CustomField4'];
+$MerchantID=$_POST['MerchantID'];
+$MerchantTradeNo=$_POST['MerchantTradeNo'];
+$PaymentDate=$_POST['PaymentDate'];
+$PaymentType=$_POST['PaymentType'];
+$PaymentTypeChargeFee=$_POST['PaymentTypeChargeFee'];
+$RtnCode=$_POST['RtnCode'];
+$RtnMsg=$_POST['RtnMsg'];
+$SimulatePaid=$_POST['SimulatePaid'];
+$StoreID=$_POST['StoreID'];
+$TradeAmt=$_POST['TradeAmt'];
+$TradeDate=$_POST['TradeDate'];
+$TradeNo=$_POST['TradeNo'];
 // 重新整理回傳參數。
 $arParameters = $_POST;
 foreach ($arParameters as $keys => $value) {
