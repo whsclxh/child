37438e24f01c406294815e3721cb9424c0f041b2
hsa90807 <[hsa90807@gmail.com]>
2019-07-10 17:11:33 +0800

    710MM

---
+++ green/E.php
@@ -0,0 +1,25 @@
+<?php
+require_once 'ECPay.Payment.Integration.php';
+
+$obj = new \ECPay_AllInOne();
+ 
+//服務參數
+$obj->ServiceURL  = $_POST['ActionURL'];
+$obj->HashKey     = $_POST['HashKey'];
+$obj->HashIV      = $_POST['HashIV'];
+$obj->MerchantID  = $_POST['MerchantID'];
+ 
+//
+$obj->Send['MerchantTradeNo'] = $_POST['MerchantTradeNo'];
+$obj->Send['ReturnURL'] = $_POST['ReturnURL'];
+$obj->Send['TimeStamp'] = $_POST['TimeStamp'];
+$obj->Send['OrderResultURL'] = $_POST['ReturnURL'];
+//訂單的商品資料
+ 
+//產生訂單(auto submit至ECPay)
+//$obj->CheckOut();
+$Response = (string)$obj->CheckOutString();
+echo $Response;
+ 
+// 自動將表單送出
+echo '<script>document.getElementById("__ecpayForm").submit();</script>';
