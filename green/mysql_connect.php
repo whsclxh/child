799c0672af737b70fa8ac4179f7a5821d174ce98
hsa90807 <[hsa90807@gmail.com]>
2019-07-10 15:43:19 +0800

    7/10-CC

--- green/mysql_connect.php
+++ green/mysql_connect.php
@@ -1,5 +1,5 @@
 <?php
 $link = mysqli_connect("localhost", "whsclxh", "1234", "green");
-mysqli_query($qaq,"SET NAMES utf8");
+mysqli_query($link,"SET NAMES utf8");
 if (!$link) die('failed to connect to database');
 ?>
