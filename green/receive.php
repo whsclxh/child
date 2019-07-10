e401a94d5c7d8aa537e3e556d237353065747a44
hsa90807 <[hsa90807@gmail.com]>
2019-07-10 11:57:21 +0800

    7/10I

--- green/receive.php
+++ green/receive.php
@@ -1,6 +1,41 @@
 <?php
+require_once 'ECPay.Payment.Integration.php';
+ 
+define( 'ECPay_MerchantID', '2000132' );
+define( 'ECPay_HashKey', '5294y06JbISpM5x9' );
+define( 'ECPay_HashIV', 'v77hoKGq4kWxNNIS' );
+ 
+// 重新整理回傳參數。
+$arParameters = $_POST;
+foreach ($arParameters as $keys => $value) {
+    if ($keys != 'CheckMacValue') {
+        if ($keys == 'PaymentType') {
+            $value = str_replace('_CVS', '', $value);
+            $value = str_replace('_BARCODE', '', $value);
+            $value = str_replace('_CreditCard', '', $value);
+        }
+        if ($keys == 'PeriodType') {
+            $value = str_replace('Y', 'Year', $value);
+            $value = str_replace('M', 'Month', $value);
+            $value = str_replace('D', 'Day', $value);
+        }
+        $arFeedback[$keys] = $value;
+    }
+}
+ 
+// 計算出 CheckMacValue
+$CheckMacValue = ECPay_CheckMacValue::generate( $arParameters, ECPay_HashKey, ECPay_HashIV );
+ 
+// 必須要支付成功並且驗證碼正確
+if ( $_POST['RtnCode'] =='1' && $CheckMacValue == $_POST['CheckMacValue'] ){
+    // 
+    // 
+    // 
+    echo "666";
+}
+ 
+// 接收到資訊回應綠界
 echo '1|OK';
-print_r($_POST);
 ?>
 <!---<!DOCTYPE html>
 <html lang="en">
