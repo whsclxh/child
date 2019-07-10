6612976e853222550b52e2919b84aaee6bbb2597
hsa90807 <[hsa90807@gmail.com]>
2019-07-10 11:43:54 +0800

    7/10G

--- green/receive.php
+++ green/receive.php
@@ -1,3 +1,84 @@
 <?php
 print_r($_POST);
 ?>
+<!---<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <div style="border: 0 auto">
+    <table border="1">
+        <tr>
+            <td>CheckMacValue</td>
+            <td><?php echo "$CheckMacValue"; ?></td>
+        </tr>
+        <tr>
+            <td>CustomField1</td>
+            <td><?php echo "$CustomField1"; ?></td>
+        </tr>
+        <tr>
+            <td>CustomField2</td>
+            <td><?php echo "$CustomField2"; ?></td>
+        </tr>
+        <tr>
+            <td>CustomField3</td>
+            <td><?php echo "$CustomField3"; ?></td>
+        </tr>
+        <tr>
+            <td>CustomField4</td>
+            <td><?php echo "$CustomField4"; ?></td>
+        </tr>
+        <tr>
+            <td>MerchantID</td>
+            <td><?php echo "$MerchantID"; ?></td>
+        </tr>
+        <tr>
+            <td>MerchantTradeNo</td>
+            <td><?php echo "$MerchantTradeNo"; ?></td>
+        </tr>
+        <tr>
+            <td>PaymentDate</td>
+            <td><?php echo "$PaymentDate"; ?></td>
+        </tr>
+        <tr>
+            <td>PaymentType</td>
+            <td><?php echo "$PaymentType"; ?></td>
+        </tr>
+        <tr>
+            <td>PaymentTypeChargeFee</td>
+            <td><?php echo "$PaymentTypeChargeFee"; ?></td>
+        </tr>
+        <tr>
+            <td>RtnCode</td>
+            <td><?php echo "$RtnCode"; ?></td>
+        </tr>
+        <tr>
+            <td>RtnMsg</td>
+            <td><?php echo "$RtnMsg"; ?></td>
+        </tr>
+        <tr>
+            <td>SimulatePaid</td>
+            <td><?php echo "$SimulatePaid"; ?></td>
+        </tr>
+        <tr>
+            <td>StoreID</td>
+            <td><?php echo "$StoreID"; ?></td>
+        </tr>
+        <tr>
+            <td>TradeAmt</td>
+            <td><?php echo "$TradeAmt"; ?></td>
+        </tr>
+        <tr>
+            <td>TradeDate</td>
+            <td><?php echo "$TradeDate"; ?></td>
+        </tr>
+        <tr>
+            <td>TradeNo</td>
+            <td><?php echo "$TradeNo"; ?></td>
+        </tr>
+    </table>
+    </div>
+</body>
+</html>
