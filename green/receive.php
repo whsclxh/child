96b72175ceceb3e33f4aabe3ddeda8c705ab0a75
hsa90807 <[hsa90807@gmail.com]>
2019-07-10 14:39:10 +0800

    7/10-S

--- green/receive.php
+++ green/receive.php
@@ -44,16 +44,13 @@ $CheckMacValue = ECPay_CheckMacValue::generate( $arParameters, ECPay_HashKey, EC
  
 // 必須要支付成功並且驗證碼正確
 if ( $_POST['RtnCode'] =='1' && $CheckMacValue == $_POST['CheckMacValue'] ){
-    // 
-    // 
-    // 
-    echo "666";
+    
 }
 else{
 	echo "not match ";
 } 
 // 接收到資訊回應綠界
-echo '1|OK';
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -65,70 +62,87 @@ echo '1|OK';
     <div style="border: 0 auto">
     <table border="1">
         <tr>
+        	<td>檢查碼</td>
             <td>CheckMacValue</td>
             <td><?php echo "$CheckMacValue"; ?></td>
         </tr>
         <tr>
+        	<td>自訂名稱欄位1</td>
             <td>CustomField1</td>
             <td><?php echo "$CustomField1"; ?></td>
         </tr>
         <tr>
+        	<td>自訂名稱欄位2</td>
             <td>CustomField2</td>
             <td><?php echo "$CustomField2"; ?></td>
         </tr>
         <tr>
+        	<td>自訂名稱欄位3</td>
             <td>CustomField3</td>
             <td><?php echo "$CustomField3"; ?></td>
         </tr>
         <tr>
+        	<td>自訂名稱欄位4</td>
             <td>CustomField4</td>
             <td><?php echo "$CustomField4"; ?></td>
         </tr>
         <tr>
+        	<td>特店編號</td>
             <td>MerchantID</td>
             <td><?php echo "$MerchantID"; ?></td>
         </tr>
         <tr>
+        	<td>特店交易編號</td>
             <td>MerchantTradeNo</td>
             <td><?php echo "$MerchantTradeNo"; ?></td>
         </tr>
         <tr>
+        	<td>付款時間</td>
             <td>PaymentDate</td>
             <td><?php echo "$PaymentDate"; ?></td>
         </tr>
         <tr>
+        	<td>特店選擇的付款方式</td>
             <td>PaymentType</td>
             <td><?php echo "$PaymentType"; ?></td>
         </tr>
         <tr>
+        	<td>通路費</td>
             <td>PaymentTypeChargeFee</td>
             <td><?php echo "$PaymentTypeChargeFee"; ?></td>
         </tr>
         <tr>
+        	<td>交易狀態</td>
             <td>RtnCode</td>
             <td><?php echo "$RtnCode"; ?></td>
         </tr>
         <tr>
+        	<td>交易訊息</td>
             <td>RtnMsg</td>
             <td><?php echo "$RtnMsg"; ?></td>
         </tr>
         <tr>
+        	<td>是否為模擬付款</td>
             <td>SimulatePaid</td>
             <td><?php echo "$SimulatePaid"; ?></td>
         </tr>
         <tr>
+        	<td>特店旗下店舖代號</td>
             <td>StoreID</td>
             <td><?php echo "$StoreID"; ?></td>
         </tr>
         <tr>
+        	<td>交易金額</td>
             <td>TradeAmt</td>
             <td><?php echo "$TradeAmt"; ?></td>
         </tr>
         <tr>
+        	<td>訂單成立時間</td>
             <td>TradeDate</td>
             <td><?php echo "$TradeDate"; ?></td>
         </tr>
         <tr>
+        	<td>綠界的交易編號</td>
             <td>TradeNo</td>
             <td><?php echo "$TradeNo"; ?></td>
         </tr>
@@ -136,3 +150,6 @@ echo '1|OK';
     </div>
 </body>
 </html>
+<?php
+echo '1|OK';
+?>
