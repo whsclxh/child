6bab0a7e4038707782eb0806740e30319978f394
hsa90807 <[hsa90807@gmail.com]>
2019-07-11 17:37:25 +0800

    711BBB

--- green/info.php
+++ green/info.php
@@ -1,4 +1,12 @@
 <?php
+include("mysql_connect.php");
 $ItemName=$_POST['ItemName'];
 echo "$ItemName";
+echo gettype($ItemName);
+$PL = "SELECT * FROM product_list where cost>99 ORDER BY cost+0 ASC";
+$result = mysqli_query($link,$PL);
+$num_rows = mysqli_num_rows($result);
+$row = mysqli_fetch_array($result);
+echo "$row[0]";
+echo gettype($row[0]);
 ?>
