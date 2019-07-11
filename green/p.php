9e8bcb1bb9ec5aa35b13d7d1f3d098dcef58a480
hsa90807 <[hsa90807@gmail.com]>
2019-07-11 15:25:25 +0800

    711BB

--- green/p.php
+++ green/p.php
@@ -179,6 +179,29 @@ $num_rows = mysqli_num_rows($result);
          });
         }
     </script>
+    <script type="text/javascript">
+        
+    function getRandom(){
+        return Math.floor(Math.random()*15)+5;
+    };
+    var chars = ['0','1','2','3','4','5','6','7','8','9',
+                'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+                'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+    function generateMixed() {
+        var res = "";
+        for(var i = 0; i < getRandom() ; i++ ) {
+            var id = Math.ceil(Math.random()*61);
+            res += chars[id];
+        }
+        return res;
+    }    
+    var a=generateMixed();
+    window.onload=function(){
+        var tradeno=document.getElementById('MerchantTradeNo');
+        tradeno.value=a;
+    };
+    </script>
 </head>
 <style>
     .right ul li {
