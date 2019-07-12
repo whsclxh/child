23ad1e9470af2d5212f8a1b10aedf638a5ea5cdc
1900542 <shaosyunliao@systex.com>
2019-07-12 16:56:55 +0800

    Update purchase.php

--- green/purchase.php
+++ green/purchase.php
@@ -104,17 +104,6 @@ $num_rows = mysqli_num_rows($result);
 </style>
 <script type="text/javascript" src="jquery-3.4.1.min.js"></script>
     <script type="text/javascript" src="pagination.js"></script>
-    <script>
-    $("#button").click(function(){
-            var check=$("input[name='ItemName[]']:checked").length;//判斷有多少個方框被勾選
-            if(check==0){
-                alert("您尚未勾選任何項目");
-                return false;//不要提交表單
-            }else{
-                return true;//提交表單
-            }
-        })
-    </script>
     <script type="text/javascript">
         function getRandom(){
         return Math.floor(Math.random()*15)+5;
@@ -167,7 +156,7 @@ $num_rows = mysqli_num_rows($result);
             var tdNode3=trNode.insertCell();
             tdNode3.innerHTML='<?php echo "$row[1]"; ?>';
             var tdNode4=trNode.insertCell();
-            tdNode4.innerHTML='<td><input type=\"checkbox\" name=\"ItemName[]\" <?php echo"value=\"$row[0]\""; ?>></td>';
+            tdNode4.innerHTML='<td><input type=\"checkbox\" name=\"ItemName[]\" data-rule=\"checked\" <?php echo"value=\"$row[0]\""; ?>></td>';
             <?php } ?>
             document.getElementById("div1").appendChild(tableNode);//添加到那个位置
             var table = document.getElementById("table");
