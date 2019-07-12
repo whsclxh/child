e8ed3f5c962dd90a5acf30ab0d18084b5b9014ae
hsa90807 <[hsa90807@gmail.com]>
2019-07-12 10:45:59 +0800

    712AA

--- green/purchase.php
+++ green/purchase.php
@@ -1,119 +1,15 @@
-<?php
+<?php 
 include("mysql_connect.php");
-$PL = "SELECT * FROM product_list  ORDER BY cost DESC+0";
+$PL = "SELECT * FROM product_list  ORDER BY cost+0 DESC";
 $result = mysqli_query($link,$PL);
-$row_num = mysqli_num_rows($result);
+$num_rows = mysqli_num_rows($result);
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
+<script src="https://apps.bdimg.com/libs/jquery/1.10.2/jquery.min.js"></script>
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width"/>
-<script src="https://apps.bdimg.com/libs/jquery/1.10.2/jquery.min.js"></script>
-    <script type="text/javascript">
-        //全局变量
-        var numCount;       //数据总数量
-        var columnsCounts;  //数据列数量
-        var pageCount;      //每页显示的数量
-        var pageNum;        //总页数
-        var currPageNum ;   //当前页数
-
-        //页面标签变量
-        var blockTable;
-        var preSpan;
-        var firstSpan;
-        var nextSpan;
-        var lastSpan;
-        var pageNumSpan;
-        var currPageSpan;
-
-
-        window.onload=function(){
-            //页面标签变量
-            tableNode=document.createElement("table");//获得对象
-            tableNode.setAttribute("id","table");
-            tableNode.setAttribute("cellspacing","0");
-            tableNode.setAttribute("class","table table-striped");
-            //上面确定了 现在开始创建
-            <?php for($x=1;$x< $row_num;$x++){ ?>
-            <?php $row = mysqli_fetch_array($result);?>
-            var trNode=tableNode.insertRow();
-            var tdNode1=trNode.insertCell();
-            tdNode1.innerHTML='<?php echo "$x"; ?>';
-            var tdNode2=trNode.insertCell();
-            tdNode2.innerHTML='<?php echo "$row[0]" ?>';
-            var tdNode3=trNode.insertCell();
-            tdNode3.innerHTML='<?php echo "$row[1]" ?>';
-            var tdNode4=trNode.insertCell();
-            tdNode4.innerHTML='<td><input type=\"radio\" name=\"ItemName\" <?php echo"value=\"$row[0]\""; ?>></td>';
-            <?php } ?>
-            document.getElementById("div1").appendChild(tableNode);//添加到那个位置
-            var table = document.getElementById("table");
-            var header = table.createTHead();
-            var row = header.insertRow(0);
-            var cell0 = row.insertCell(0);
-            cell0.innerHTML = "<b>序號</b>";
-            var cell1 = row.insertCell(1);
-            cell1.innerHTML = "<b>商品</b>";
-            var cell2 = row.insertCell(2);            
-            cell2.innerHTML = "<b>價錢</b>";
-            var cell3 = row.insertCell(3);            
-            cell3.innerHTML = "<b>選擇</b>";
-            blockTable = document.getElementById("table");
-            preSpan = document.getElementById("spanPre");
-            firstSpan = document.getElementById("spanFirst");
-            nextSpan = document.getElementById("spanNext");
-            lastSpan = document.getElementById("spanLast");
-            pageNumSpan = document.getElementById("spanTotalPage");
-            currPageSpan = document.getElementById("spanPageNum");
-
-            numCount = document.getElementById("table").rows.length - 1;       //取table的行数作为数据总数量（减去标题行1）
-            columnsCounts = blockTable.rows[0].cells.length;
-            pageCount = 10;
-            pageNum = parseInt(numCount/pageCount);
-            if(0 != numCount%pageCount){
-                pageNum += 1;
-            }
-
-            firstPage();
-            $(function() {
-        /* For zebra striping */
-        $("table tr:nth-child(odd)").addClass("odd-row");
-        /* For cell text alignment */
-        $("table td:first-child, table th:first-child").addClass("first");
-        /* For removing the last border */
-        $("table td:last-child, table th:last-child").addClass("last");
-         });
-    };
-    </script>
-    <style type="text/css">
-        #hor-zebra
-{
-    font-family: "Lucida Sans Unicode", "Lucida Grande", Sans-Serif;
-    font-size: 12px;
-    margin: 45px;
-    width: 480px;
-    text-align: left;
-    border-collapse: collapse;
-}
-#hor-zebra th
-{
-    font-size: 14px;
-    font-weight: normal;
-    padding: 10px 8px;
-    color: #039;
-}
-#hor-zebra td
-{
-    padding: 8px;
-    color: #669;
-}
-#hor-zebra .odd
-{
-    background: #e8edff; 
-}
-    </style>
-    <style type="text/css">
+    <title></title><style type="text/css">
     body{
     }
     table .table-striped{
@@ -206,10 +102,10 @@ $row_num = mysqli_num_rows($result);
         background: -webkit-gradient(linear, 0% 0%, 0% 20%, from(#ededed), to(#e8eaeb));
     }
 </style>
-    <script src="pagination.js"></script>
+<script type="text/javascript" src="jquery-3.4.1.min.js"></script>
+    <script type="text/javascript" src="pagination.js"></script>
     <script type="text/javascript">
-        
-    function getRandom(){
+        function getRandom(){
         return Math.floor(Math.random()*15)+5;
     };
     var chars = ['0','1','2','3','4','5','6','7','8','9',
@@ -225,35 +121,95 @@ $row_num = mysqli_num_rows($result);
         return res;
     }    
     var a=generateMixed();
-    window.onload=function(){
-        var tradeno=document.getElementById('MerchantTradeNo');
-        tradeno.value=a;
-    };
+        //全局变量
+        var numCount;       //数据总数量
+        var columnsCounts;  //数据列数量
+        var pageCount;      //每页显示的数量
+        var pageNum;        //总页数
+        var currPageNum ;   //当前页数
+
+        //页面标签变量
+        var blockTable;
+        var preSpan;
+        var firstSpan;
+        var nextSpan;
+        var lastSpan;
+        var pageNumSpan;
+        var currPageSpan;
+
+        window.onload=function(){
+            //页面标签变量
+            var tradeno=document.getElementById('MerchantTradeNo');
+            tradeno.value=a;
+            tableNode=document.createElement("table");//获得对象
+            tableNode.setAttribute("id","table");
+            tableNode.setAttribute("cellspacing","0");
+            tableNode.setAttribute("class","table table-striped");
+            //上面确定了 现在开始创建
+            <?php for($x=1;$x< $num_rows;$x++){ ?>
+            <?php $row = mysqli_fetch_array($result);?>
+            var trNode=tableNode.insertRow();
+            var tdNode1=trNode.insertCell();
+            tdNode1.innerHTML='<?php echo "$x"; ?>';
+            var tdNode2=trNode.insertCell();
+            tdNode2.innerHTML='<?php echo "$row[0]" ?>';
+            var tdNode3=trNode.insertCell();
+            tdNode3.innerHTML='<?php echo "$row[1]"; ?>';
+            var tdNode4=trNode.insertCell();
+            tdNode4.innerHTML='<td><input type=\"radio\" name=\"ItemName\" <?php echo"value=\"$row[0]\""; ?>></td>';
+            <?php } ?>
+            document.getElementById("div1").appendChild(tableNode);//添加到那个位置
+            var table = document.getElementById("table");
+            var header = table.createTHead();
+            var row = header.insertRow(0);
+            var cell0 = row.insertCell(0);
+            cell0.innerHTML = "<b>編號</b>";
+            var cell1 = row.insertCell(1);
+            cell1.innerHTML = "<b>商品</b>";
+            var cell2 = row.insertCell(2);            
+            cell2.innerHTML = "<b>價錢</b>";
+            var cell3 = row.insertCell(3);
+            cell3.innerHTML = "<b>選擇</b>";
+            blockTable = document.getElementById("table");
+            preSpan = document.getElementById("spanPre");
+            firstSpan = document.getElementById("spanFirst");
+            nextSpan = document.getElementById("spanNext");
+            lastSpan = document.getElementById("spanLast");
+            pageNumSpan = document.getElementById("spanTotalPage");
+            currPageSpan = document.getElementById("spanPageNum");
+
+            numCount = document.getElementById("table").rows.length - 1;       //取table的行数作为数据总数量（减去标题行1）
+            columnsCounts = blockTable.rows[0].cells.length;
+            pageCount = 10;
+            pageNum = parseInt(numCount/pageCount);
+            if(0 != numCount%pageCount){
+                pageNum += 1;
+            }
+
+            firstPage();
+            $(function() {
+        /* For zebra striping */
+        $("table tr:nth-child(odd)").addClass("odd-row");
+        /* For cell text alignment */
+        $("table td:first-child, table th:first-child").addClass("first");
+        /* For removing the last border */
+        $("table td:last-child, table th:last-child").addClass("last");
+         });
+        }
     </script>
-    <script src="jquery-3.4.1.min.js"></script>
-    <style>
-        label{
-            display: block;
-            color: #F73678;
-        }
-        form{
-            width: 40%;
-            margin: 0 auto;
-        }
-    </style>
 </head>
+<style>
+    .right ul li {
+    display:inline;
+}
+</style>
 <body align="center">
-<form id="idFormAioCheckOut" method="post" action="store.php" >    
-    <div class="container" align="center">
+
+<div class="container" align="center">
     <h2 style="margin-top: 2%">商品列表</h2>
-<div id="div1"></div>
-<div id="pagiDiv" align="center" style="width:40%">
-        <span id="spanFirst">第一頁</span>  
-        <span id="spanPre">上一頁</span>  
-        <span id="spanNext">下一頁</span>  
-        <span id="spanLast">最後一頁</span>  
-        第 <span id="spanPageNum"></span> 頁/共 <span id="spanTotalPage"></span> 頁
-</div>
+    <h3 id="you"></h3>
+<form action="info.php" method="post" name="form1">
+<div id="div1">
 </div>
     <input type="hidden" name="API_URL" value="https://payment-stage.ecpay.com.tw" class="form-control"/>
     <input type="hidden" name="MerchantID" value="2000132" class="form-control"/>
@@ -271,8 +227,18 @@ $row_num = mysqli_num_rows($result);
     <input type="hidden" name="PaymentInfoURL" value="http://whsclxh.ddns.net/green/receivepayment.php" class="form-control"/>
     <input type="hidden" name="ClientRedirectURL" value="http://whsclxh.ddns.net/green/clientredirect.php" class="form-control"/>
     <input type="hidden" name="ChoosePayment" value="Credit">
-    <input type="hidden" name="CreditInstallment" value="12,24" class="form-control"/> 
-    <div align="center"><button type="submit">綠界線上支付</button></div> 
+    <input type="hidden" name="CreditInstallment" value="12,24" class="form-control"/>
+    <div align="center"><button type="submit">綠界線上支付</button></div>
 </form>
+<br>
+<div id="pagiDiv" align="center" style="width:40%">
+        <span id="spanFirst">第一頁</span>  
+        <span id="spanPre">上一頁</span>  
+        <span id="spanNext">下一頁</span>  
+        <span id="spanLast">最後一頁</span>  
+        第 <span id="spanPageNum"></span> 頁/共 <span id="spanTotalPage"></span> 頁
+</div>
+</div>
+
 </body>
 </html>
