2114844fa6ceb6a0101592cb8273d716e260a916
hsa90807 <[hsa90807@gmail.com]>
2019-07-12 10:41:11 +0800

    712Y

--- green/store.php
+++ green/store.php
@@ -66,5 +66,5 @@ $Response = (string)$obj->CheckOutString();
 
 echo "<div style=\"display:none\">$Response</div>"; 
 // 自動將表單送出
-echo '<script>document.getElementById("__ecpayForm").submit();</script>';
+/*echo '<script>document.getElementById("__ecpayForm").submit();</script>';*/
 ?>
