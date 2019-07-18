ea363e4b862a915530af347d9d71f5bfb22266fd
hsa90807 <hsa90807@gmail.com>
2019-07-18 16:04:05 +0800

    718E

--- green/receive1.php
+++ green/receive1.php
@@ -25,7 +25,7 @@ $TradeNo=$_POST['TradeNo'];
 $sql1 = "SELECT * FROM paylist WHERE Account='$Account'";
 $result1 = mysqli_query($link,$sql1);
 $paylistr = @mysqli_fetch_all($result1);
-print_r($paylistr);
+print_r($paylistr[0]);
 
 // 重新整理回傳參數。
 $arParameters = $_POST;
