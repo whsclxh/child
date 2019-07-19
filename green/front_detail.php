e1e159d4552edd3794076830fdefcf6380d95152
hsa90807 <hsa90807@gmail.com>
2019-07-19 16:05:35 +0800

    719II

--- green/front_detail.php
+++ green/front_detail.php
@@ -4,12 +4,11 @@ if($_SESSION['Account'] == null){
     echo "<script>alert('您尚未登入!');</script>";
     echo '<meta http-equiv=REFRESH CONTENT=0;url=Home.html>';
 }
-$_SESSION['detail']=$_POST;
+$_SESSION['detail']=$_POST['detail'];
 if($_SESSION['detail'] == null){
     echo "<script>alert('請重新選擇!');</script>";
     echo '<meta http-equiv=REFRESH CONTENT=0;url=purchase.php>';
 }else{
-	echo "<script>alert('0');</script>";
 	echo '<meta http-equiv=REFRESH CONTENT=0;url=detail.php>';
 }
 ?>
