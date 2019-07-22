88b4b57dcf814c2ab67f4c5ac8587b85ed1f2a72
hsa90807 <hsa90807@gmail.com>
2019-07-22 09:29:54 +0800

    722B

--- green/as.php
+++ green/as.php
@@ -8,15 +8,17 @@ $list = "SELECT * FROM front where Account = '$Account' AND cardinal='$cardinal'
             $listr = @mysqli_fetch_row($listre);
             $first=explode("#", $listr[8]);
             $c=count($first);
+            $second=arraY();
             for ($i=0;$i<$c-1;$i++) {
                 $second[$i]=explode("*",$first[$i]);
             }
-            for($j=0;$j<$c-1;$j++){
+            print_r($second);
+            /*for($j=0;$j<$c-1;$j++){
                 $l = "SELECT * FROM product_list where Product = '$second[$j][0]'";
                 $li = mysqli_query($link,$l);
                 $lis = @mysqli_fetch_row($li);
                 $third=(int)$lis[8]-(int)$second[$j][1];
             }
-print_r($third);
+print_r($third);*/
 
 ?>
