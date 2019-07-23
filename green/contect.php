b6db126ac414ca4e85429f3dafc07a9a06771aff
hsa90807 <hsa90807@gmail.com>
2019-07-23 16:46:14 +0800

    722

---
+++ green/contect.php
@@ -0,0 +1,22 @@
+<form id="form1" name="form1" method="post" action="sendmail.php">
+    <fieldset>
+    <legend>留言給我們</legend>
+    <label>您的大名：</label>
+    <input id="C_name" name="C_name" type="text" />
+    <br />
+
+    <label>您的Email：</label>
+    <input id="C_email" name="C_email" type="text" />
+    <br />
+
+    <label>您的電話號碼：</label>
+    <input id="C_tel" name="C_tel" type="text" />
+    <br />
+
+    <label>意見：</label>
+    <textarea id="C_message" name="C_message"></textarea>
+    <br />
+
+    <input id="submit" name="submit" type="submit" value="確定送出" />
+    </fieldset>
+    </form>
---
+++ green/sendEmail.php
@@ -0,0 +1,31 @@
+<?php
+    require_once('./PHPMailer/PHPMailerAutoload.php');
+    $C_name=$_POST['C_name'];
+    $C_email=$_POST['C_email'];
+    $C_tel=$_POST['C_tel'];
+    $C_message=$_POST['C_message'];
+   
+    $mail= new PHPMailer();                             //建立新物件
+    $mail->SMTPDebug = 2;                        
+    $mail->IsSMTP();                                    //設定使用SMTP方式寄信
+    $mail->SMTPAuth = true;                        //設定SMTP需要驗證
+    $mail->SMTPSecure = "ssl";                    // Gmail的SMTP主機需要使用SSL連線
+    $mail->Host = "smtp.gmail.com";             //Gamil的SMTP主機
+    $mail->Port = 465;                                 //Gamil的SMTP主機的埠號(Gmail為465)。
+    $mail->CharSet = "utf-8";                       //郵件編碼
+    $mail->Username = "hsa20127";       //Gamil帳號
+    $mail->Password = "sam20127";                 //Gmail密碼
+    $mail->From = "hsa20127@gmail.com";        //寄件者信箱
+    $mail->FromName = "XXX";                  //寄件者姓名
+    $mail->Subject ="這是一封測試信"; //郵件標題
+    $mail->Body = "親愛的 ".$C_name."(".$C_email.")，您好：<br /><br />電話:".$C_tel."<br />回應內容:".$C_message; //郵件內容
+    $mail->addAttachment('../uploadfile/file/dirname.png','new.jpg'); //附件，改以新的檔名寄出
+    $mail->IsHTML(true);                             //郵件內容為html
+    $mail->AddAddress("$C_email");            //收件者郵件及名稱
+    if(!$mail->Send()){
+        echo "Error: " . $mail->ErrorInfo;
+    }else{
+        echo "<b>感謝您的留言，您的建議是我們前進的動力!</b>";
+    }
+    
+?>
