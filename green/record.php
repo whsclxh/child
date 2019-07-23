1ec0732a57b42f6c83623e59e07741a9102c3892
hsa90807 <hsa90807@gmail.com>
2019-07-23 11:12:07 +0800

    Update record.php

--- green/record.php
+++ green/record.php
@@ -134,7 +134,7 @@ $paylistrow = mysqli_num_rows($result);
     </script>
     <script>
     $(document).ready(function(){
-        $("#order").click(function(){
+        $("#order").on("click",(function(){
             alert('a');
             var order=$(this).val();
             var order_input=document.createElement("input");
