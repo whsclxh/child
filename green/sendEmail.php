3a6937f9cecf1359c3f42371547ee6170ebc30be
hsa90807 <hsa90807@gmail.com>
2019-07-24 09:37:05 +0800

    724

--- green/sendEmail.php
+++ green/sendEmail.php
@@ -1,12 +1,28 @@
 <?php
+    include("mysql_connect.php");
     include("class.phpmailer.php"); //匯入PHPMailer類別
     require 'PHPMailer/src/Exception.php';
     require 'PHPMailer/src/SMTP.php';
-    $C_name=$_POST['C_name'];
-    $C_email=$_POST['C_email'];
-    $C_tel=$_POST['C_tel'];
-    $C_message=$_POST['C_message'];
-   
+    function get_Password(){
+        $chars = ['0','1','2','3','4','5','6','7','8','9',
+                'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+                'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+                for($i=0;$i<10;$i++){
+                    $id = rand(0,61);
+                $res .= $chars[$id];
+                }
+                return $res;
+    }
+    $account=$_POST['account'];
+    $email=$_POST['email'];
+    $find="SELECT * FROM user WHERE Account='$account' AND Email='$email'";
+    $result=mysqli_query($link,$find);
+    $row=mysqli_fetch_row($result);
+    if($row==null){
+        echo "<script>alert('找不到此帳號or郵件地址!');</script>";
+        echo '<meta http-equiv=REFRESH CONTENT=0;url=ForgetPassword.html>';
+    }
+    $password=get_Password();
     $mail= new PHPMailer();                             //建立新物件
     $mail->SMTPDebug = 2;                        
     $mail->IsSMTP();                                    //設定使用SMTP方式寄信
@@ -15,18 +31,21 @@
     $mail->Host = "smtp.gmail.com";             //Gamil的SMTP主機
     $mail->Port = 465;                                 //Gamil的SMTP主機的埠號(Gmail為465)。
     $mail->CharSet = "utf-8";                       //郵件編碼
-    $mail->Username = "hsa20127";       //Gamil帳號
-    $mail->Password = "sam20127";                 //Gmail密碼
-    $mail->From = "hsa20127@gmail.com";        //寄件者信箱
-    $mail->FromName = "XXX";                  //寄件者姓名
-    $mail->Subject ="這是一封測試信"; //郵件標題
-    $mail->Body = "親愛的 ".$C_name."(".$C_email.")，您好：<br /><br />電話:".$C_tel."<br />回應內容:".$C_message; //郵件內容
+    $mail->Username = "zxc1900542";       //Gamil帳號
+    $mail->Password = "qwe1900542";                 //Gmail密碼
+    $mail->From = "zxc1900542@gmail.com";        //寄件者信箱
+    $mail->FromName = "password";                  //寄件者姓名
+    $mail->Subject ="----------------------------請確認您的密碼----------------------------"; //郵件標題
+    $mail->Body = "請確認您的密碼並記下"."<br>帳號:".$row[0]."<br>密碼:".$password;//郵件內容
     $mail->IsHTML(true);                             //郵件內容為html
-    $mail->AddAddress("$C_email");            //收件者郵件及名稱
+    $mail->AddAddress("$email");            //收件者郵件及名稱
     if(!$mail->Send()){
         echo "Error: " . $mail->ErrorInfo;
+        echo '<meta http-equiv=REFRESH CONTENT=1;url=ForgetPassword1.html>';
+        echo "<script>alert('寄信發生錯誤：'. $mail->ErrorInfo);</script>";
     }else{
-        echo "<b>感謝您的留言，您的建議h是我們前進的動 力!</b>";
+        echo '<meta http-equiv=REFRESH CONTENT=1;url=Home.php>';
+        echo "<script>alert('已送出信件!請到信箱收信並重新登入並修改密碼');</script>";
     }
     
 ?>
