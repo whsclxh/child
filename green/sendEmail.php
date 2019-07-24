e8af77cad5c97cc67e003de51257416c94137e13
hsa90807 <hsa90807@gmail.com>
2019-07-24 11:53:02 +0800

    Update sendEmail.php

--- green/sendEmail.php
+++ green/sendEmail.php
@@ -1,8 +1,6 @@
 <?php
     include("mysql_connect.php");
     include("class.phpmailer.php"); //匯入PHPMailer類別
-    require 'PHPMailer/src/Exception.php';
-    require 'PHPMailer/src/SMTP.php';
     function get_Password(){
         $chars = ['0','1','2','3','4','5','6','7','8','9',
                 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
